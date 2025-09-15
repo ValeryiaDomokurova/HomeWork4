00686ba5b5dfbb2ed33d82f82d0914cf05cd878d
Valeryia <lprilepa0@gmail.com>
2025-09-15 23:41:30 +0300

    Fix Homework7

--- homeworks/hw7/bulls_and_cows/hw7_solution.py
+++ homeworks/hw7/bulls_and_cows/hw7_solution.py
@@ -1,32 +1,38 @@
 import random
 
 SECRET_NUM = ''.join(random.sample('0123456789', 4))
-print("Компьютер загадал число")
 
-while True:
-    gamer = input("Введи 4 цифры без повторений: ")
+def play_game():
+    print("Компьютер загадал число")
 
-    if len(gamer) != 4 or not gamer.isdigit():
-        print("Нужно ввести 4 цифры ")
-        continue
+    while True:
+        gamer = input("Введи 4 цифры без повторений: ")
 
-    if len(set(gamer)) != 4:
-        print("Цифры не должны повторяться ")
-        continue
+        if len(gamer) != 4 or not gamer.isdigit():
+            print("Нужно ввести 4 цифры ")
+            continue
 
-    BULLS = 0
-    for i in range(4):
-        if gamer[i] == SECRET_NUM[i]:
-            BULLS += 1
+        if len(set(gamer)) != 4:
+            print("Цифры не должны повторяться ")
+            continue
 
-    COWS = 0
-    for x in gamer:
-        if x in SECRET_NUM:
-            COWS += 1
-    COWS -= BULLS
+        BULLS = 0
+        COWS = 0
 
-    if BULLS == 4:
-        print(f"Поздравляю! Ты угадал число {SECRET_NUM}!")
-        break
+        for i in range(4):
+            if gamer[i] == SECRET_NUM[i]:
+                BULLS += 1
 
-    print(f"Быки: {BULLS} , Коровы: {COWS}")
+        for x in gamer:
+            if x in SECRET_NUM:
+                COWS += 1
+
+        COWS -= BULLS
+
+        if BULLS == 4:
+            print(f"Поздравляю! Ты угадал число {SECRET_NUM}!")
+            break
+
+        print(f"Быки: {BULLS} , Коровы: {COWS}")
+
+play_game()
