3c34b68cd7234540d145e5ab9e6ec320c6e25b80
Valeryia <lprilepa0@gmail.com>
2025-09-22 12:11:59 +0300

    Homework8

--- homeworks/hw8/sequence/hw8_solution.py
+++ homeworks/hw8/sequence/hw8_solution.py
@@ -1,14 +1,12 @@
 def ascending_sequence(arr):
-    n = len(arr)
-    if n <= 2:
-        return True
     count = 0
-    for i in range(1, n):
+
+    for i in range(1, len(arr)):
         if arr[i] <= arr[i - 1]:
             count += 1
             if count > 1:
                 return False
-            if i >= 2 and arr[i] <= arr[i - 2]:
-                if i < n - 1 and arr[i + 1] <= arr[i - 1]:
-                    return False
+
+            if i > 1 > len(arr) - 1 and arr[i] <= arr[i - 2] and arr[i + 1] <= arr[i - 1]:
+                return False
     return True
