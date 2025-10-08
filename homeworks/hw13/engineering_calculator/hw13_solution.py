23af5b1960d02c2db896e75eabe5fea185422df2
Valeryia <lprilepa0@gmail.com>
2025-10-08 18:04:43 +0300

    Fix homework

--- homeworks/hw13/engineering_calculator/hw13_solution.py
+++ homeworks/hw13/engineering_calculator/hw13_solution.py
@@ -18,9 +18,12 @@ def evaluate_expression(expression):
     validation_result = validate_expression(expression)
     if not validation_result[0]:
         if validation_result[1] == "Syntax error in the expression.":
-            return "Syntax error in the expression."
-        return f"Invalid char --> {validation_result[1]}"
+            error_message = "Syntax error in the expression."
+        else:
+            error_message = f"Invalid char --> {validation_result[1]}"
+        return error_message
 
+# I've tried many options without eval, but this one is the most optimal and there is error
     try:
         result = eval(expression)
         if isinstance(result, float) and result.is_integer():
@@ -29,8 +32,6 @@ def evaluate_expression(expression):
     except ZeroDivisionError:
         return "Division by zero."
     except SyntaxError:
-        return "Syntax error in the expression."
-    except NameError:
         return "Syntax error in the expression."
 
 
