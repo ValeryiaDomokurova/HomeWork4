8f027ed9e8bd23e761b1e95e7c9236af0c565e85
Valeryia <lprilepa0@gmail.com>
2025-11-08 18:21:19 +0300

    Homework 23

---
+++ homeworks/hw23.py
@@ -0,0 +1,38 @@
+'''
+
+Login screen (https://www.saucedemo.com/)
+
+~ Copy Username "standard_user" (selector [id="login_credentials"])
+~ Enter the copied data (selector [name="user-name"])
+~ Copy Password "secret_sauce" (selector [class=login_password])
+~ Enter the copied data (selector [name="user-name"])
+~ Press the "Log in" button (selector [class="submit-button btn_action"])
+~ Successfully navigated to the next page(https://www.saucedemo.com/inventory.html)
+
+Main page screen (https://www.saucedemo.com/inventory.html)
+
+~ Press the 'Add to Cart' button (selector [id=add-to-cart-sauce-labs-backpack])
+~ Press the 'Add to Cart' button ([selector id=add-to-cart-sauce-labs-bike-light])
+~ Press the cart icon (selector [class=shopping_cart_link])
+~ Successfully navigated to the next page (https://www.saucedemo.com/cart.html)
+
+Cart screen(https://www.saucedemo.com/cart.html)
+
+~ Press the 'Checkout' button (selector [class="btn btn_action btn_medium checkout_button "])
+~ Successfully navigate to the next page (https://www.saucedemo.com/checkout-step-one.html)
+
+Checkout: Your Information screen(https://www.saucedemo.com/checkout-step-one.html)
+
+~ Enter your first name (selector [id="first-name"])
+~ Enter your last name (selector [id="last-name"])
+~ Enter your postal code (selector [id="postal-code"])
+~ Press the 'Continue' button (selector [id="continue"])
+~ Successfully navigated to the next page (https://www.saucedemo.com/checkout-step-two.html)
+
+Checkout: Overview screen(https://www.saucedemo.com/checkout-step-two.html)
+
+~ Check the information
+~ Press the 'Finish' button (selector [id="finish"])
+~ Successfully navigated to the next page (https://www.saucedemo.com/checkout-complete.html)
+
+'''
